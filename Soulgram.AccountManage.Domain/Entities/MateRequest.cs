88066f4ba277bd549af9b44fb356cc146f41802d
Дима Киреев <dimanoit@gmail.com>
2022-05-08 18:33:00 +0300

    update materequest model

--- Soulgram.AccountManage.Domain/Entities/MateRequest.cs
+++ Soulgram.AccountManage.Domain/Entities/MateRequest.cs
@@ -2,8 +2,8 @@ namespace Soulgram.AccountManage.Domain.Entities;
 
 public class MateRequest
 {
-    public string SenderId { get; set; }
-    public string RecipientId { get; set; }
+    public string SenderId { get; set; } = null!;
+    public string? RecipientId { get; set; }
     public MateRequestStatus Status { get; set; }
     public DateTime CreationDate { get; set; }
 }
