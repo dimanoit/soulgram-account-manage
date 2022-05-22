fe88e62de3a1a12d319e4b9379bb34de4adc5b49
Дима Киреев <dimanoit@gmail.com>
2022-05-22 20:07:02 +0300

    add logging

--- Soulgram.AccountManage.Api/Program.cs
+++ Soulgram.AccountManage.Api/Program.cs
@@ -3,14 +3,19 @@ using Soulgram.AccountManage.Infrastracture;
 using Soulgram.AccountManage.Infrastracture.IntegrationEvents;
 using Soulgram.AccountManage.Persistence;
 using Soulgram.Eventbus.Interfaces;
+using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host
+    .ConfigureLogging((_, logging) => logging.ClearProviders())
+    .UseSerilog();
+
 var configuration = builder.Configuration;
 
 builder.Services.AddPersistence(configuration);
 builder.Services.AddInfrastructure(configuration);
 builder.Services.AddControllers();
-builder.Services.AddApplication();
+builder.Services.AddApplication(configuration);
 
 var app = builder.Build();
 
