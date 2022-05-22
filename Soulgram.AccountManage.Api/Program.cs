59d8b4a6f4de0ed79e9b0feb7cc103fa46f1e779
dimanoit <dimanoit@gmail.com>
2022-05-22 13:59:47 +0300

    feat: add logging to account manage

--- Soulgram.AccountManage.Api/Program.cs
+++ Soulgram.AccountManage.Api/Program.cs
@@ -1,3 +1,4 @@
+using Serilog;
 using Soulgram.AccountManage.Application;
 using Soulgram.AccountManage.Infrastracture;
 using Soulgram.AccountManage.Infrastracture.IntegrationEvents;
@@ -5,12 +6,16 @@ using Soulgram.AccountManage.Persistence;
 using Soulgram.Eventbus.Interfaces;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Host
+    .ConfigureLogging((_, logging) => logging.ClearProviders())
+    .UseSerilog();
+
 var configuration = builder.Configuration;
 
 builder.Services.AddPersistence(configuration);
 builder.Services.AddInfrastructure(configuration);
 builder.Services.AddControllers();
-builder.Services.AddApplication();
+builder.Services.AddApplication(configuration);
 
 var app = builder.Build();
 
--- Soulgram.AccountManage.Application/ServiceInjector.cs
+++ Soulgram.AccountManage.Application/ServiceInjector.cs
@@ -1,16 +1,33 @@
 using System.Reflection;
 using MediatR;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Soulgram.AccountManage.Application.Commands;
 using Soulgram.AccountManage.Application.Services;
+using Soulgram.Logging;
+using Soulgram.Logging.Models;
 
 namespace Soulgram.AccountManage.Application;
 
 public static class ServiceInjector
 {
-    public static void AddApplication(this IServiceCollection serviceCollection)
+    public static void AddApplication(
+        this IServiceCollection serviceCollection,
+        IConfiguration configuration)
     {
+        serviceCollection.AddLogging(configuration);
         serviceCollection.AddMediatR(typeof(CreateUserCommand).GetTypeInfo().Assembly);
         serviceCollection.AddTransient<IMateService, MateService>();
+    }
+
+    private static IServiceCollection AddLogging(
+        this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var loggingSettings = configuration
+            .GetSection("LoggingSettings")
+            .Get<LoggingSettings>();
+
+        return services.AddLogging(loggingSettings);
     }
 }
